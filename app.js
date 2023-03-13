51f62f5b3a3bf097a4e0ac969e79007f096fbb82
albertofernadez <albertofernandezbaena@outlook.com>
2023-03-13 10:09:01 +0100

    examen

--- app.js
+++ app.js
@@ -1,93 +1,37 @@
-//Insertar datos
-document.addEventListener("DOMContentLoaded", function () {
-    fetch("https://raw.githubusercontent.com/FRomero999/ExamenDIW2022/main/rutas_arqueologicas.json?")
-    .then(response => response.json())
-        .then(data => {
-            const tabla = document.querySelector("table tbody");
-            data.forEach(cliente => {
-                const row = tabla.insertRow();
-                });
-        })
-        .catch(error => console.error(error));
-});
+var mapa = L.map('mapa').setView([36.72071131817986, -4.420041081375409], 13);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(mapa);
+
+fetch('https://raw.githubusercontent.com/FRomero999/ExamenDIW2022/main/rutas_arqueologicas.json')
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+        console.log(data[24].properties.nombre);
+        const tbody = document.querySelector("#listaElementos");
+        const template = document.querySelector('#elementoLista');
+        for (let i = 0; i < data.length; i++) {
+            const clone = template.content.cloneNode(true);
+            let nombre = clone.getElementById('nombre')
+            let horario = clone.getElementById('horario')
+            let direccion = clone.getElementById('direccion')
+            let telefono = clone.getElementById('numerotelefono')
+            nombre.textContent = data[i].properties.nombre;
+            horario.textContent = data[i].properties.horario;
+            direccion.textContent = data[i].properties.direccion;
+            telefono.textContent = data[i].properties.telefono;
+            if (telefono.textContent != "") {
+                telefono.classList.add('tarjeta')
+            }
+            tbody.appendChild(clone);
+
+            var marcador = L.marker([data[i].properties.x, data[i].properties.y]).addTo(mapa);
+            marcador.bindPopup(`${data[i].properties.nombre}`);
+        }
 
 
-//Resube los datos
-const btnCargar = document.querySelector("#btnAñadir");
-
-btnCargar.addEventListener("click", function () {
-    fetch("https://raw.githubusercontent.com/FRomero999/ExamenDIW2022/main/rutas_arqueologicas.json?")
-        .then(response => response.json())
-        .then(data => {
-
-            const tabla = document.querySelector("table tbody");
-            data.forEach(cliente => {
-                const row = tabla.insertRow();
-            });
-        })
-        .catch(error => console.log(error));
-});
+    })
 
 
-var boton
-var lista
-var plantilla
-var map
 
-const options = {
-  enableHighAccuracy: true,
-  maximumAge: 30000,
-  timeout: 27000
-}
-
-function test(){
-  console.log("test")
-}
-
-function test2(){
-
-  navigator.geolocation.getCurrentPosition( 
-    (coordenadas)=>{ 
-      let nuevo = plantilla.content.cloneNode(true)
-      let texto = ""
-
-      texto = texto + coordenadas.coords.latitude +","
-      texto = texto + coordenadas.coords.longitude
-
-      nuevo.querySelector("li").title = texto
-      nuevo.querySelector("span.lat").innerText = coordenadas.coords.latitude
-      nuevo.querySelector("span.long").innerText = coordenadas.coords.longitude
-      
-      lista.appendChild(nuevo)
-
-      map.setView([coordenadas.coords.latitude, coordenadas.coords.longitude], 18)
-      L.marker([coordenadas.coords.latitude, coordenadas.coords.longitude]).addTo(map)
-    },
-    (err)=>{ console.log(err)},
-    options
-  )
-  console.log("final")
-
-}
-
-window.addEventListener("load", ()=>{
-  
-  boton = document.querySelector("button")
-  lista  = document.querySelector("ul")
-  plantilla = document.querySelector("template")
-
-  map = L.map('map').setView([51.505, -0.09], 13)
-  L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      maxZoom: 19,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-  }).addTo(map)
-
-  boton.addEventListener("click", (ev)=>{
-    console.log(this)
-    console.log(ev)
-    console.log("click")
-    boton.classList.add("rojo")
-  })
-
-  boton.addEventListener("click",test2)  
-})
